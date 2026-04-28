b62a0cf304b1ddc603ac08e66cbc755c7ac33725
xiangyan93 <yan.xiang@duke.edu>
2026-04-28 15:22:21 -0400

    fix: Use PreTrainedTokenizer base class to fix SMILES tokenization
    BertTokenizer in transformers v5 uses a Rust backend that bypasses the
    Python _tokenize() override, causing all multi-token SMILES to be
    mapped to <pad> (unknown). Switching to PreTrainedTokenizer ensures the
    SMILES regex tokenizer is used by tokenize(), encode(), and __call__().

--- molformer/tokenizer.py
+++ molformer/tokenizer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from transformers import BertTokenizer
+from transformers import PreTrainedTokenizer
 import regex as re
 
 
 PATTERN = r"(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
 
 
-class MolTranBertTokenizer(BertTokenizer):
+class MolTranBertTokenizer(PreTrainedTokenizer):
     def __init__(self, vocab_file: str = '',
                  do_lower_case=False,
                  unk_token='<pad>',
@@ -16,22 +16,44 @@ class MolTranBertTokenizer(BertTokenizer):
                  cls_token='<bos>',
                  mask_token='<mask>',
                  **kwargs):
-        super().__init__(vocab_file,
-                         unk_token=unk_token,
-                         sep_token=sep_token,
-                         pad_token=pad_token,
-                         cls_token=cls_token,
-                         mask_token=mask_token,
-                         **kwargs)
+        self._vocab = {}
+        self._ids_to_tokens = {}
+        with open(vocab_file, 'r') as f:
+            for idx, line in enumerate(f):
+                token = line.strip()
+                self._vocab[token] = idx
+                self._ids_to_tokens[idx] = token
 
         self.regex_tokenizer = re.compile(PATTERN)
-        self.wordpiece_tokenizer = None
-        self.basic_tokenizer = None
+
+        super().__init__(
+            unk_token=unk_token,
+            sep_token=sep_token,
+            pad_token=pad_token,
+            cls_token=cls_token,
+            mask_token=mask_token,
+            **kwargs,
+        )
+
+    @property
+    def vocab(self):
+        return dict(self._vocab)
+
+    @property
+    def vocab_size(self):
+        return len(self._vocab)
+
+    def get_vocab(self):
+        return dict(self._vocab)
 
     def _tokenize(self, text):
-        split_tokens = self.regex_tokenizer.findall(text)
-        return split_tokens
+        return self.regex_tokenizer.findall(text)
+
+    def _convert_token_to_id(self, token):
+        return self._vocab.get(token, self._vocab.get(self.unk_token, 0))
+
+    def _convert_id_to_token(self, index):
+        return self._ids_to_tokens.get(index, self.unk_token)
 
     def convert_tokens_to_string(self, tokens):
-        out_string = "".join(tokens).strip()
-        return out_string
+        return "".join(tokens).strip()
